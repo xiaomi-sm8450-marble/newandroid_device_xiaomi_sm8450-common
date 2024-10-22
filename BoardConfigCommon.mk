89d4fccd062d29d8786813c01f22eb2e7fa618a0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-22 16:07:12 +0530

    sm8450-common: Add support for Dolby Atmos

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -249,6 +249,7 @@ endif
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/vintf/device_framework_matrix.xml \
+    $(COMMON_PATH)/vintf/device_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
